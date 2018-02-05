10339693531328529c519a6b94c5a789a95c06bb
Luca <lucaciabo@hotmail.it>
2018-02-05 23:14:20 +0100

    Fixed small bugs

--- Main.py
+++ Main.py
@@ -13,12 +13,20 @@ for k in range(0,5):
     print("-------");
     print("HTRU_2 DATASET");
     cv.holdoutCrossValidation(htruDataSet);
-    print("------");
-    print("DOROTHEA");
-    cv.test(dorotheaTrain,dorotheaTest);
     print("");
     print("");
-
-
+print("------");
+print("DOROTHEA");
+cv.test(dorotheaTrain,dorotheaTest);
+print("");
+print("");
+print("------K-FOLD CROSS VALIDATION--------")
+print("Banknote")
 banknoteDataSet=dt.DataSet(filename='dataSets/data_banknote_authentication.txt');
-cv.kFoldCrossValidation(5,banknoteDataSet)
+cv.kFoldCrossValidation(5,banknoteDataSet);
+print("HTRU")
+htruDataSet=dt.DataSet(filename='dataSets/HTRU_2.txt');
+cv.kFoldCrossValidation(5,htruDataSet);
+
+
+
